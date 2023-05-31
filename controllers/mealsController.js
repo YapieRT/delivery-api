4cff0f7558e9151f8eda72da6c42adb40dc73674
Sokolov Valentyn <46300336+YapieRT@users.noreply.github.com>
2023-05-31 14:06:56 +0300

    Update mealsController.js

--- controllers/mealsController.js
+++ controllers/mealsController.js
@@ -1,22 +1,5 @@
 import Meal from '../models/mealModel.js';
 
-// {MealsController.addMeal();
-
-// const data = new Meal({
-//   name: 'Mushroom Mac and Cheese.',
-//   restaurant: '647520188151be5099837120',
-// });
-
-// export const addMeal = async (req, res) => {
-//     await Meal.create(data)
-//       .then((MealResponse) => {
-//         console.log(MealResponse); // Handle the saved order document
-//       })
-//       .catch((error) => {
-//         console.error(error); // Handle the error if the insertion fails
-//       });
-//   };}
-
 export const getRestaurantMeals = async (req, res) => {
   try {
     const restaurantId = req.query.id;
